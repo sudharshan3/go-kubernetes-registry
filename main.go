384ec929deda1dac8f151b61e62dd3cd4c728330
Sudharshan Muralidharan <50818126+sudharshan3@users.noreply.github.com>
2023-07-07 19:26:56 +0530

    code: Code for creating Registry Deployment
    Added code to create deployment for registry
    
    Fixes: #6
    
    Signed-off-by: Sudharshan Muralidharan <sudharshanrampoore@gmail.com>

--- main.go
+++ main.go
@@ -4,14 +4,29 @@ import (
 	"log"
 
 	"github.com/sudharshan3/go-kubernetes-registry/kubeconnector"
+	"github.com/sudharshan3/go-kubernetes-registry/registry"
 )
 
 func main() {
+	var newRegistry registry.Registry
+	var replicas int32 = 1
 	log.Print("Connecting to Kubernetes Cluster...")
 	kube, err := kubeconnector.Connect()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Print("Connection Established Successfully...")
-	log.Print(kube)
+	log.Print("Create Registry Deployment...")
+	newRegistry.RegistryName = "registry"
+	newRegistry.RegistryAppName = "registry"
+	newRegistry.RegistryNamespace = "default"
+	newRegistry.RegistryImage = "registry:2"
+	newRegistry.RegistryPort = 5000
+	newRegistry.RegistryReplicas = &replicas
+	newRegistry.RegistryMountPath = "/var/lib/registry"
+	_, err = newRegistry.CreateRegistryDeployment(kube)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
